401a65f5bee67ae37423ca2a04407072d239a5f4
Xeno <226741508+XenoLeads@users.noreply.github.com>
2025-11-06 10:42:48 +0530

    feat(desktop): add statistics tab's quick view/actions

--- src/scripts/statistics-template.js
+++ src/scripts/statistics-template.js
@@ -1,6 +1,8 @@
+const desktop_quick_view_actions_sidebar = document.getElementsByClassName("desktop-quick-view-actions-sidebar")[0];
+
 function get_statistics_template() {
   return `
-  <button class="button export-statistics-button">Export</button>
+        <button class="button export-statistics-button">Export</button>
         <div class="card statistics-time-filter-container">
           <div class="statistics-time-filters">
             <button class="button statistics-time-filter statistics-time-filter-all-time selected">All Time</button>
@@ -18,7 +20,24 @@ function get_statistics_template() {
   `;
 }
 
-function init_statistics_template(callback) {}
+function init_statistics_template() {
+  desktop_quick_view_actions_sidebar.innerHTML = `
+          <div class="deskto-statistics-quick-view-actions-container">
+            <button class="button desktop-export-statistics-button">Export</button>
+            <div class="card statistics-time-filter-container">
+              <h2 class="statistics-time-filter-heading">Time</h2>
+              <div class="separator"></div>
+              <div class="statistics-time-filters">
+                <button class="button statistics-time-filter statistics-time-filter-all-time selected">All Time</button>
+                <button class="button statistics-time-filter statistics-time-filter-today">Today</button>
+                <button class="button statistics-time-filter statistics-time-filter-this-week">This Week</button>
+                <button class="button statistics-time-filter statistics-time-filter-this-month">This Month</button>
+                <button class="button statistics-time-filter statistics-time-filter-this-year">This Year</button>
+              </div>
+            </div>
+          </div>
+  `;
+}
 
 export default {
   name: "statistics",
